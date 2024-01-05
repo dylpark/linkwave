53d8240ea39403412c06dd46ff0cae2edabb7719
Dylan Park <64296934+dylpark@users.noreply.github.com>
2024-01-05 17:25:58 +1000

    Creating skeleton multi-step form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<button class="btn btn-primary">One</button>
-<button class="btn btn-secondary">Two</button>
-<button class="btn btn-accent btn-outline">Three</button>
