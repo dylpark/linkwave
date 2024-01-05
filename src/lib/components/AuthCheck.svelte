5a63256b15a266ed5f7155abae8939dc154d5dd2
Dylan Park <64296934+dylpark@users.noreply.github.com>
2024-01-05 18:47:38 +1000

    Creating AuthCheck component

---
+++ src/lib/components/AuthCheck.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { user } from '$lib/firebase';
+</script>
+
+{#if user}
+    <slot />
+{:else}
+    <p class="text-error my-10">Log in to view this page</p>
+    <a class="btn btn-primary" href="/login">
+        Sign in
+    </a>
+{/if}
